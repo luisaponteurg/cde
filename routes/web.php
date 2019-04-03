16e64629537bd6fcea46eec6e2676c4c2a1ae90c
jhonnyprz <jhonnyjose1998ii@gmail.com>
2019-04-03 00:31:37 -0400

    name of routes fine

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,23 @@ Route::get('/', function () {
     return view('auth.acceder');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// RUTAS DE ACCESO
+	// Authentication Routes...
+    Route::get('acceder', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('acceder', 'Auth\LoginController@login');
+    Route::post('salir', 'Auth\LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    Route::get('registrar', 'Auth\RegisterController@showRegistrationForm')->name('registrar');
+    Route::post('registrar', 'Auth\RegisterController@register');
+
+    // Password Reset Routes...
+    Route::get('contraseña/resetear', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('contraseña/correo', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('contraseña/resetear/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('contraseña/resetear', 'Auth\ResetPasswordController@reset');
+// RUTAS DE ACCESO
+
+Route::get('/Dace', 'HomeController@index')->name('home');
