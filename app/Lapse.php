9f271ff249ebf7aa0b950daabf58a680336468ac
Yusuke1998 <jhonnyjose1998@gmail.com>
2019-04-04 11:13:24 -0400

    modelo de nuevas tablas

--- app/Lapse.php
+++ app/Lapse.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lapse extends Model
 {
-    //
+    protected $fillable = ['type_lapse_id', 'completion_date', 'start_date'];
 }
--- app/Type_lapse.php
+++ app/Type_lapse.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Type_lapse extends Model
 {
-    //
+	protected $fillable = ['description'];
+
 }
